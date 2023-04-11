a5b586ca8d02d4ee4fb319284a372a93bb951bbb
MrVoltss <virmrvolts@gmail.com>
2023-04-11 00:07:55 +0000

    added databases

--- processdatabase/chunk_data.py
+++ processdatabase/chunk_data.py
@@ -6,75 +6,93 @@ from uuid import uuid4
 from tqdm.auto import tqdm
 import json
 import random
+from tqdm.auto import tqdm
 
-# Source directory and filename
-source_directory = 'source'
-input_filename = f'{source_directory}/input_data.json'
+def load_input_data(input_filename):
+    with open(input_filename, 'r') as f:
+        text = f.read()
+    return [{'text': text}]
 
-# Folder and filename to store the data
-user_folder = 'user1'
-output_filename = f'{user_folder}/{user_folder}_data.jsonl'
+def main():
+    # Define source directory
+    current_directory = os.path.abspath(os.path.dirname(__file__))
+    parent_directory = os.path.dirname(current_directory)
+    source_directory = os.path.join(parent_directory, 'sampledata')
 
-##############################################
-Main code
-##############################################
+    # Get all input file paths in the source directory
+    input_filepaths = [os.path.join(source_directory, f) for f in os.listdir(source_directory) if os.path.isfile(os.path.join(source_directory, f))]
 
-# Where the data is stored
-data = []
-# Where the chunks are stored
-chunks = []
+    # Process each input file
+    for input_filepath in input_filepaths:
+        # Load the data from the input file
+        data = load_input_data(input_filepath)
 
-# cl100k is for gpt-3.5-turbo and gpt-4
-tokenizer = tiktoken.get_encoding('cl100k_base')
+        # Get user_folder name
+        user_folder = os.path.splitext(os.path.basename(input_filepath))[0]
 
-# create the length function find lengh of text in tokens
-def tiktoken_len(text):
-    tokens = tokenizer.encode(
+        # Folder and filename to store the data
+        output_filename = f'{user_folder}/{user_folder}_data.jsonl'
+
+        # Process the input data and save it in the required format
+        process_data_and_save_output(data, user_folder, output_filename)
+
+        # Delete the processed input file
+        os.remove(input_filepath)
+
+def process_data_and_save_output(data, user_folder, output_filename):
+    if isinstance(data, str):
+        data = [{'text': data}]
+        
+    chunks = []
+
+    # Use the p50k_base tokenizer
+    tokenizer = tiktoken.get_encoding('cl100k_base')
+
+    # create the length function find lengh of text in tokens
+    def tiktoken_len(text):
+        tokens = tokenizer.encode(
         text,
         disallowed_special=()
+        )
+        return len(tokens)
+
+    # Split the text into chunks of x tokens with y token overlap
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=20,  # number of tokens overlap between chunks
+        length_function=tiktoken_len,
+        separators=["\n\n", "\n", " ", ""]
     )
-    return len(tokens)
+        
+    for idx, record in enumerate(tqdm(data)):
+        chunks.extend([{
+            'id': str(uuid4()),
+            'text': t,
+            'chunk': i,
+            'channel': user_folder
+        } for i, t in enumerate(text_splitter.split_text(record['text']))])
 
-# Split the text into chunks of x tokens with y token overlap
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=400,
-    chunk_overlap=20,  # number of tokens overlap between chunks
-    length_function=tiktoken_len,
-    separators=['\n\n', '\n', ' ', '']
-)
 
-# Split the text into chunks
-for idx, record in enumerate(tqdm(data)):
-    texts = text_splitter.split_text(record['text'])
+    # Create the user folder if it doesn't exist
+    os.makedirs(user_folder, exist_ok=True)
 
-    # Include the timestamp at the start of the chunk
-    chunks.extend([{
-        'id': str(uuid4()),
-        'text': "{" + texts[i],
-        'chunk': i,
-        'channel': record['channel']
-    } for i in range(len(texts))])
+    # Save the chunks to the jsonl file
+    if os.path.exists(output_filename):
+        # If the file exists, append the new chunks
+        with open(output_filename, 'a') as f:
+            for chunk in chunks:
+                f.write(json.dumps(chunk) + '\n')
+    else:
+        # If the file doesn't exist, create a new one and save the chunks
+        with open(output_filename, 'w') as f:
+            for chunk in chunks:
+                f.write(json.dumps(chunk) + '\n')
+
+    print(f"File: {output_filename}")
+    print("5 random elements in chunks list:")
+    for i in random.sample(range(len(chunks)), 5):
+        print(f"{i+1}. ID: {chunks[i]['id']}, Channel: {chunks[i]['channel']}, Chunk: {chunks[i]['chunk']}\nText:\n{chunks[i]['text']}\n")
     
-# Create the user folder if it doesn't exist
-os.makedirs(user_folder, exist_ok=True)
 
-# Save the chunks to the jsonl file
-if os.path.exists(output_filename):
-    # If the file exists, append the new chunks
-    with open(output_filename, 'a') as f:
-        for chunk in chunks:
-            f.write(json.dumps(chunk) + '\n')
-else:
-    # If the file doesn't exist, create a new one and save the chunks
-    with open(output_filename, 'w') as f:
-        for chunk in chunks:
-            f.write(json.dumps(chunk) + '\n')
-        
-        
-        
-        
-    
-# Print 5 random elements from the chunks list with full text
-print("5 random elements in chunks list:")
-for i in random.sample(range(len(chunks)), 5):
-    print(f"{i+1}. ID: {chunks[i]['id']}, Channel: {chunks[i]['channel']}, Chunk: {chunks[i]['chunk']}\nText:\n{chunks[i]['text']}\n")
+if __name__ == "__main__":
+    main()
